de381354257de6781b8cd2cfc0d04c780aff5b7a
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-04-19 12:04:17 -0700

    adds newData method and inputs

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import DataNew from '../views/DataNew.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/new',
+    name: 'boat-datadata-new',
+    component: DataNew
   },
   {
     path: '/about',
---
+++ src/views/DataNew.vue
@@ -0,0 +1,36 @@
+<script>
+
+
+export default {
+  data: function () {
+    return {
+      message: "Welcome to add boat data",
+      boats: {},
+      newBoatParams: {}
+
+    };
+  },
+  created: function () { },
+  methods: {
+    newData: function () {
+      console.log('adding data');
+      this.boat.push(this.newBoatParams);
+      console.log(this.boat)
+      this.newBoatParams = {}
+    }
+  },
+};
+</script>
+
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <p>Name: <input v-model="newBoatParams.name"></p>
+    <p>Model: <input v-model="newBoatParams.model"></p>
+    <p>Description: <input v-model="newBoatParams.description"></p>
+    <p><button v-on:click="newData()">Add Data</button></p>
+  </div>
+</template>
+
+<style>
+</style>
